4787e7260dc0e5ee7f1270dcb0d6d30dda223649
abdelrahman-tarek-0 <y.p.challenges@gmail.com>
2022-06-17 23:38:55 +0200

    update: added route quality

--- API/src/handlers/youtube.controller.js
+++ API/src/handlers/youtube.controller.js
@@ -5,40 +5,46 @@ const quality = async (req, res, next) => {
       const url = req.query.url
       let quality = []
       if (ytdl.validateURL(url)) {
-        let info = await ytdl.getInfo(url);
-        for(let i = 0; i < info.formats.length; i++) {
-            quality.push({quality:info.formats[i].qualityLabel,itag:info.formats[i].itag})
-        }
-        // remove duplicated quality label , itag and null in the array
-        quality = quality.filter((item, index, arr) => {
-            return arr.map(mapItem => mapItem.quality).indexOf(item.quality) === index && arr.map(mapItem => mapItem.itag).indexOf(item.itag) === index && item.quality !== null
-        })
-        console.log(quality);
+         let info = await ytdl.getInfo(url)
+
+         for (let i = 0; i < info.formats.length; i++) {
+            if (
+               info.formats[i].hasVideo &&
+               info.formats[i].hasAudio &&
+               info.formats[i].qualityLabel !== null
+            ) {
+               quality.push({
+                  quality: info.formats[i].qualityLabel,
+                  itag: info.formats[i].itag,
+               })
+            }
+         }
+            res.json({quality: quality})
+        
       }
    } catch {
       next(Error)
    }
 }
 const mp4 = async (req, res, next) => {
-    try {
-       const url = req.query.url
-       const quality = req.query.quality
-       if (ytdl.validateURL(url)) {
-          ytdl(url)
+   try {
+      const url = req.query.url
+      const quality = req.query.quality
+      if (ytdl.validateURL(url)) {
+         ytdl(url, {
+            quality: quality
+         })
             .on('info', (info) => {
                res.header(
                   'Content-Disposition',
                   `attachment; filename="${info.videoDetails.title}.mp4"`
                )
             })
-            .on('error', (error) => {
-               return res.status(400)
-            })
             .pipe(res)
-       }
-    } catch {
-       next(Error)
-    }
- }
+      }
+   } catch {
+      next(Error)
+   }
+}
 
-module.exports = { mp4 }
+module.exports = { mp4, quality }
--- API/src/routes/APIs/youtube.routes.js
+++ API/src/routes/APIs/youtube.routes.js
@@ -3,5 +3,7 @@ const routes = express.Router()
 const controller = require('../../handlers/youtube.controller.js')
 
 routes.get('/mp4', controller.mp4)
+routes.get('/quality', controller.quality)
+
 
 module.exports = routes
