4ef060b13c2afbc2311bb03d06f292a1f0560a77
abdelrahman-tarek-0 <y.p.challenges@gmail.com>
2022-06-19 01:38:41 +0200

    style: added function for better format

--- FrontEnd/js/scripts.js
+++ FrontEnd/js/scripts.js
@@ -1,32 +1,33 @@
-const btn = document.querySelector('#btn')
-let url = ''
 const backEnd = 'http://localhost:3000'
+const searchBtn = document.querySelector('#btn')
+const youtubePlaceholder = document.querySelector('#youtubePlaceholder')
 const botnsContainer = document.querySelector('#quality')
+let url = ''
 
-btn.addEventListener('click', async () => {
+
+searchBtn.addEventListener('click', async () => {
+   // rest of the code
+   youtubePlaceholder.innerHTML = ''
+   botnsContainer.innerHTML = ''
+   document.querySelector('#url').focus()
+
    try {
       url = document.querySelector('#url').value
+      if (!url) {alert('please enter a url');return}
 
-      if (!url) {
-         alert('please enter a url')
-         return
-      }
+      let BackEndURL = videoOrAudioURL(quality,'qualityRoute')
+      const res = await fetch(BackEndURL)
 
-      let VideoBackEndUrl = ''
-      if (document.querySelector('#format').selectedIndex === 0) {
-         VideoBackEndUrl = `${backEnd}/api/Youtube/quality?url=${url}&format=audio`
-      } else if (document.querySelector('#format').selectedIndex === 1) {
-         VideoBackEndUrl = `${backEnd}/api/Youtube/quality?url=${url}&format=video`
-      }
-
-      const res = await fetch(VideoBackEndUrl)
       if (res.ok) {
          const data = await res.json()
          updateQualityUi(data.quality)
       } else {
          const error = await res.json()
          alert('error: ' + error.message + '\nstatus: ' + res.status)
+         preLoader.style.display = 'none'
       }
+     
+
    } catch (error) {
       if (error.message === 'Failed to fetch') {
          alert('server is down :(\n please try again later')
@@ -38,40 +39,48 @@ btn.addEventListener('click', async () => {
 
 const updateQualityUi = (data) => {
    const scrVideo = `https://www.youtube.com/embed/${url.split('=')[1]}`
-
-   const youtubePlaceholder = document.querySelector('#youtubePlaceholder')
    youtubePlaceholder.innerHTML = `<iframe width="420" height="315" id="video" src="${scrVideo}"></iframe>`
-
    botnsContainer.innerHTML = ''
-
    for (let i = 0; i < data.length; i++) {
       botnsContainer.innerHTML += `<button class="${data[i].itag} botns">${data[i].quality}</button>`
    }
 }
 // after click on any new botns , it will download the video
+
 botnsContainer.addEventListener('click', async (e) => {
    if (e.target.classList.contains('botns')) {
       try{
          const quality = e.target.classList[0]
-         let VideoBackEndUrl = ''
-
-         if (document.querySelector('#format').selectedIndex === 0) {
-            VideoBackEndUrl = `${backEnd}/api/Youtube/mp3?url=${url}&quality=${quality}`
-            console.log(VideoBackEndUrl);
-         } else if (document.querySelector('#format').selectedIndex === 1) {
-            VideoBackEndUrl = `${backEnd}/api/Youtube/mp4?url=${url}&quality=${quality}`
-         }
-
-         const res = await fetch(VideoBackEndUrl)
+         const BackEndURL = videoOrAudioURL(quality,'downloadRoute')
+         const res = await fetch(BackEndURL)
          if (res.ok) {
-            window.open(VideoBackEndUrl)
+            window.open(BackEndURL)
          } else {
             alert('something went wrong')
          }
       }catch(error){
-         alert('')
+         alert(`error: ${error.message}`)
       }
-      
-
    }
 })
+
+const videoOrAudioURL = (quality,route)=>{
+   let YoutubeBackEndURL = ''
+   if (route === 'qualityRoute') {
+      if (document.querySelector('#format').selectedIndex === 0) {
+         YoutubeBackEndURL = `${backEnd}/api/Youtube/quality?url=${url}&format=audio`
+         return YoutubeBackEndURL
+      } else if (document.querySelector('#format').selectedIndex === 1) {
+         YoutubeBackEndURL = `${backEnd}/api/Youtube/quality?url=${url}&format=video`
+         return YoutubeBackEndURL
+      }
+   }else if(route === 'downloadRoute'){
+      if (document.querySelector('#format').selectedIndex === 0) {
+         YoutubeBackEndURL = `${backEnd}/api/Youtube/mp3?url=${url}&quality=${quality}`
+         return YoutubeBackEndURL
+      } else if (document.querySelector('#format').selectedIndex === 1) {
+         YoutubeBackEndURL = `${backEnd}/api/Youtube/mp3?url=${url}&quality=${quality}`
+         return YoutubeBackEndURL
+      }
+   }
+}
