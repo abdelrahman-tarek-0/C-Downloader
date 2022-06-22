632389f566b93026810cb622930b09a0ecc74cd9
abdelrahman-tarek-0 <y.p.challenges@gmail.com>
2022-06-22 08:27:02 +0200

    remove: facebook endpoints (not working need to make webscrapper to get the facebook videos my self)

--- API/src/handlers/facebook.controller.js
+++ API/src/handlers/facebook.controller.js
@@ -1,22 +1,2 @@
-const fbvid = require('fbvideos')
-
-const high = async (req, res, next) => {
-   try {
-      const url = req.query.url
-      const video = await fbvid.high(url)
-      res.json({ vid: video })
-   } catch (error) {
-      next(error)
-   }
-}
-const low = async (req, res, next) => {
-   try {
-      const url = req.query.url
-      const video = await fbvid.low(url)
-      res.json({ vid: video })
-   } catch (error) {
-      next(error)
-   }
-}
 
 module.exports = { high, low }
